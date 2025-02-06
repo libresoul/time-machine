cdf7f37ba240fd160945d450465923dc81c4171a
ireallywantthatname <rladsilva@students.nsbm.ac.lk>
2025-02-06 08:52:36 +0530

    feat: adds tailwindcdn for styling

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="styles/main.css">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=MedievalSharp&display=swap" rel="stylesheet">
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=MedievalSharp&display=swap" rel="stylesheet">
 </head>
-<body class="font-['MedievalSharp'] bg-slate-900">
+
+<body class="font-['MedievalSharp'] bg-slate-900 text-slate-100 text-2xl">
