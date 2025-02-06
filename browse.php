be684dbebc8511d5a3d506d49944862a3f5da1d5
ireallywantthatname <rladsilva@students.nsbm.ac.lk>
2025-02-06 13:07:27 +0530

    style(index.php && browse.php): changes the UI

---
+++ browse.php
@@ -0,0 +1,15 @@
+<?php
+include 'includes/config.php';
+include_once 'includes/header.inc.php';
+$result = $db->query("SELECT id, title FROM pages");
+?>
+
+<?php while ($row = $result->fetchArray(SQLITE3_ASSOC)): ?>
+    <div class="font-['MedievalSharp'] text-4xl bg-slate-950 m-4 p-4 rounded">
+        <a href="page.php?id=<?= $row['id'] ?>"><?= htmlspecialchars($row['title']) ?></a>
+    </div>
+<?php endwhile; ?>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -4,13 +4,12 @@ include_once 'includes/header.inc.php';
 $result = $db->query("SELECT id, title FROM pages");
 ?>
 
-<h1>Middle Era Wiki</h1>
-<a href="create.php">Create New Page</a>
-<ul>
-    <?php while ($row = $result->fetchArray(SQLITE3_ASSOC)): ?>
-        <li><a href="page.php?id=<?= $row['id'] ?>"><?= htmlspecialchars($row['title']) ?></a></li>
-    <?php endwhile; ?>
-</ul>
+<div class="font-['MedievalSharp'] flex flex-col justify-center items-center sm:w-full md:w-[60%]">
+    <div class="bg-slate-950 p-10 m-10 rounded text-8xl">
+        <h1 class="text-center">A Wiki For The Nerds</h1>
+    </div>
+    <p class="text-2xl text-center">Hello, time travelers! So, you are here to embark on a journey through time. Experience the marvels of the past, uncover the mysteries of the future, and see history unfold before your very eyes. Choose your timeline and start your adventure!</p>
+</div>
 </body>
 
 </html>
